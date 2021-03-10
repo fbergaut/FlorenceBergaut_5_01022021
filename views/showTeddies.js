67ce7ea3de4a02dfa16d1b806b013887379c65eb
fbergaut <f.bergaut@gmail.com>
2021-03-10 16:21:12 +0100

    Created all product.html + wrote my JS to link product to each pageProduct

--- views/showTeddies.js
+++ views/showTeddies.js
@@ -6,6 +6,8 @@
 //   }
 // };
 
+// const e = require("express");
+
 // bearsRequest.open("GET", "http://localhost:3000/api/teddies");
 // bearsRequest.send();
 
@@ -34,81 +36,83 @@ divRowProduct.setAttribute("class", "row products");
 
 let bears = getTeddies()
     .then(data =>{
-        let bears = document.getElementById("products");
-        data.map((bear) => {
-          // On crée les éléments html qui vont permettre d'afficher les datas
+      let bears = document.getElementById("products");
+      data.map((bear) => {
+        // On crée les éléments html qui vont permettre d'afficher les datas
 
-          const divProduct = document.createElement("div");
-          const aTeddyLinkProduct = document.createElement("a");
-          const divImgProduct = document.createElement("div");
-          const imgProduct = document.createElement("img");
-          const divCardProduct = document.createElement("div");
-          const h5TeddyName = document.createElement("h5");
-          const pTeddyPrice = document.createElement("p");
+        const divProduct = document.createElement("div");
+        const aTeddyLinkProduct = document.createElement("a");
+        const divImgProduct = document.createElement("div");
+        const imgProduct = document.createElement("img");
+        const divCardProduct = document.createElement("div");
+        const h5TeddyName = document.createElement("h5");
+        const pTeddyPrice = document.createElement("p");
 
-          // On attribut des noms de class à tous les éléments créés afin de les stylisés
+        // On attribut des noms de class à tous les éléments créés afin de les stylisés
 
-          divProduct.setAttribute("class", "col-12 col-lg-3");
-          aTeddyLinkProduct.setAttribute("class", "productsLink");
-          divImgProduct.setAttribute("class", "card border-0 bearProduct");
-          imgProduct.setAttribute("class", "card-img-top");
-          divCardProduct.setAttribute("class", "card-body bg-white");
-          h5TeddyName.setAttribute("class", "card-title text-center");
-          pTeddyPrice.setAttribute("class", "card-text text-center");
+        divProduct.setAttribute("class", "col-12 col-lg-3");
+        aTeddyLinkProduct.setAttribute("class", "productsLink");
+        divImgProduct.setAttribute("class", "card border-0 bearProduct");
+        imgProduct.setAttribute("class", "card-img-top");
+        divCardProduct.setAttribute("class", "card-body bg-white");
+        h5TeddyName.setAttribute("class", "card-title text-center");
+        pTeddyPrice.setAttribute("class", "card-text text-center");
 
-          // On définit la source de l'élément <img> à imageUrl et l'attribut alt est renseigné avec le nom du produit
+        // On définit la source de l'élément <img> à imageUrl et l'attribut alt est renseigné avec le nom du produit
 
-          imgProduct.src = bear.imageUrl;
-          imgProduct.alt = bear.name;
+        imgProduct.src = bear.imageUrl;
+        imgProduct.alt = bear.name;
+
+        // imgProduct.style.width = '220px';
+        // imgProduct.style.height = "270px";
+
+        // On affiche le nom des Teddies ainsi que leurs prix
+
+        h5TeddyName.innerHTML = bear.name;
+        pTeddyPrice.innerHTML = bear.price + " €";
+
+        // On ajoute les élments au DOM pour créer la partie présentation de produit sur index.html
+
+        bears.appendChild(divRowProduct);
+
+        divRowProduct.appendChild(divProduct);
+
+        divProduct.appendChild(aTeddyLinkProduct);
+
+        aTeddyLinkProduct.appendChild(divImgProduct);
+        aTeddyLinkProduct.appendChild(divCardProduct);
+
+        divImgProduct.appendChild(imgProduct);
+
+        divCardProduct.appendChild(h5TeddyName);
+        divCardProduct.appendChild(pTeddyPrice);
+
+      });
+
+      // On récupère tous les liens produits, on les transforme en tableau et à l'évènement onClick, on va vers la page produit du teddy concerné.
+
+      const liens = document.querySelectorAll(".productsLink");
+      const liensArray = Array.from(liens);
+
+      liensArray[0].addEventListener("click", function () {
+        location.href = "product-norbert.html";
+      });
+      liensArray[1].addEventListener("click", function () {
+        location.href = "product-arnold.html";
+      });
+      liensArray[2].addEventListener("click", function () {
+        location.href = "product-lenny-et-carl.html";
+      });
+      liensArray[3].addEventListener("click", function () {
+        location.href = "product-gustav.html";
+      });
+      liensArray[4].addEventListener("click", function () {
+        location.href = "product-garfunkel.html";
+      });
 
 
-          // imgProduct.style.width = '220px';
-          // imgProduct.style.height = "270px";
-
-          // On affiche le nom des Teddies ainsi que leurs prix
-
-          h5TeddyName.innerHTML = bear.name;
-          pTeddyPrice.innerHTML = bear.price + " €";
-
-          // On ajoute les élments au DOM pour créer la partie présentation de produit sur index.html
-
-          bears.appendChild(divRowProduct);
-
-          divRowProduct.appendChild(divProduct);
-
-          divProduct.appendChild(aTeddyLinkProduct);
-
-          aTeddyLinkProduct.appendChild(divImgProduct);
-          aTeddyLinkProduct.appendChild(divCardProduct);
-
-          divImgProduct.appendChild(imgProduct);
-
-          divCardProduct.appendChild(h5TeddyName);
-          divCardProduct.appendChild(pTeddyPrice);
-        } )
-})
+    })
 
 
 
 
-
-
-// function afficherLesProduits(responseProduct, div) {
-//   let bears = document.getElementById("products");
-//   responseProduct.map((bear) => {
-//     const div = document.createElement("div");
-//     div.innerHTML = bear.name;
-//     bears.appendChild(div);
-//   });
-// };
-
-// afficherLesProduits(responseProduct, "bears");
-
-// let responseBears = [];
-
-// function afficherLesProduits(responseBears) {
-//   for (var i = 0; i < responseBears.length; i++) {
-//     var produit = responseBears[i];
-//     console.log(produit.name + produit.price);
-//   }
-// }
