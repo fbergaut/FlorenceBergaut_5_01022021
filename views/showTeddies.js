21d9723f4e36ec43e20a2a08e654b545822107a3
fbergaut <f.bergaut@gmail.com>
2021-03-10 14:25:35 +0100

    object-fit on img to ift the div without squashing the img

--- views/showTeddies.js
+++ views/showTeddies.js
@@ -15,7 +15,8 @@
 //   .then((response) => alert(JSON.stringify(response)))
 //   .catch((error) => alert("Erreur : " + error));
 
-//liste tous les teddies de la base
+// Connecte à l'API et récupère tous les teddies de la base
+
 async function getTeddies(){
     let rep = await fetch("http://localhost:3000/api/teddies", {
       method: "GET",
@@ -24,51 +25,67 @@ async function getTeddies(){
     return reponse;
 }
 
+// On crée un élément <div> et lui attribut un nom de class afin d'acceuillir les autres éléments qui vont être créés un peu plus bas
+
 const divRowProduct = document.createElement("div");
 divRowProduct.setAttribute("class", "row products");
+
+// On stock dans la variable bears les datas récupérées de l'API et on les boucle avec map() afin d'afficher chaque teddy
 
 let bears = getTeddies()
     .then(data =>{
         let bears = document.getElementById("products");
         data.map((bear) => {
-        const divProduct = document.createElement("div");
-        const aTeddyLinkProduct = document.createElement("a");
-        const divImgProduct = document.createElement("div");
-        const imgProduct = document.createElement("img");
-        const divCardProduct = document.createElement("div");
-        const h5TeddyName = document.createElement("h5");
-        const pTeddyPrice = document.createElement("p");
-        
-        
-        divProduct.setAttribute("class", "col-12 col-lg-2");
-        aTeddyLinkProduct.setAttribute("class", "productsLink");
-        divImgProduct.setAttribute("class", "card border-0 bearProduct");
-        imgProduct.setAttribute("class", "card-img-top");
-        divCardProduct.setAttribute("class", "card-body bg-white");
-        h5TeddyName.setAttribute("class", "card-title text-center");
-        pTeddyPrice.setAttribute("class", "card-text text-center");
+          // On crée les éléments html qui vont permettre d'afficher les datas
 
-        imgProduct.src = bear.imageUrl;
-        imgProduct.alt = bear.name;
+          const divProduct = document.createElement("div");
+          const aTeddyLinkProduct = document.createElement("a");
+          const divImgProduct = document.createElement("div");
+          const imgProduct = document.createElement("img");
+          const divCardProduct = document.createElement("div");
+          const h5TeddyName = document.createElement("h5");
+          const pTeddyPrice = document.createElement("p");
 
-        h5TeddyName.innerHTML = bear.name;
-        pTeddyPrice.innerHTML = bear.price.toPrecision(4) + " €";
-        
-        bears.appendChild(divRowProduct);
-        divRowProduct.appendChild(divProduct);
-        divProduct.appendChild(aTeddyLinkProduct);
+          // On attribut des noms de class à tous les éléments créés afin de les stylisés
 
-        aTeddyLinkProduct.appendChild(divImgProduct);
-        aTeddyLinkProduct.appendChild(divCardProduct);
-        
+          divProduct.setAttribute("class", "col-12 col-lg-3");
+          aTeddyLinkProduct.setAttribute("class", "productsLink");
+          divImgProduct.setAttribute("class", "card border-0 bearProduct");
+          imgProduct.setAttribute("class", "card-img-top");
+          divCardProduct.setAttribute("class", "card-body bg-white");
+          h5TeddyName.setAttribute("class", "card-title text-center");
+          pTeddyPrice.setAttribute("class", "card-text text-center");
 
-        divImgProduct.appendChild(imgProduct);
+          // On définit la source de l'élément <img> à imageUrl et l'attribut alt est renseigné avec le nom du produit
 
-        divCardProduct.appendChild(h5TeddyName);
-        divCardProduct.appendChild(pTeddyPrice);
+          imgProduct.src = bear.imageUrl;
+          imgProduct.alt = bear.name;
 
-        
-         } )
+
+          // imgProduct.style.width = '220px';
+          // imgProduct.style.height = "270px";
+
+          // On affiche le nom des Teddies ainsi que leurs prix
+
+          h5TeddyName.innerHTML = bear.name;
+          pTeddyPrice.innerHTML = bear.price.toPrecision(4) + " €";
+
+          // On ajoute les élments au DOM pour créer la partie présentation de produit sur index.html
+
+          bears.appendChild(divRowProduct);
+
+          divRowProduct.appendChild(divProduct);
+
+          divProduct.appendChild(aTeddyLinkProduct);
+
+          aTeddyLinkProduct.appendChild(divImgProduct);
+          aTeddyLinkProduct.appendChild(divCardProduct);
+
+          divImgProduct.appendChild(imgProduct);
+
+          divCardProduct.appendChild(h5TeddyName);
+          divCardProduct.appendChild(pTeddyPrice);
+        } )
 })
 
 
